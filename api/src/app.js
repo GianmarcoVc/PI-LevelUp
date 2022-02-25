4bf7a949ceb8ad65e34a41b3fca44bbd63e468e4
GianmarcoVc <gianmarcovalentinc@gmail.com>
2022-02-24 21:25:00 -0500

    add access origins

--- api/src/app.js
+++ api/src/app.js
@@ -9,13 +9,16 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  const allowedOrigins = ['videogames-app-nu.vercel.app', 'videogames-app-git-master-gianmarcovc.vercel.app', 'videogames-app-gianmarcovc.vercel.app']
+  const { origin } = req.headers;
+  if(allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
